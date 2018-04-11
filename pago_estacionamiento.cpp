2b936044bd39755d5ee8396dfa92da5f1627c06e
Leonardo Ibarra Palacios <libarra0@ucol.mx>
2018-04-10 19:07:48 -0500

    Segunda tabla

--- pago_estacionamiento.cpp
+++ pago_estacionamiento.cpp
@@ -35,6 +35,17 @@ void casos_de_prueba(){
     cout << "10       " << " " << "90             "<<  " " <<pago_estacionamiento(10)<< " " <<endl;
     cout << "12       " << " " << "84             "<<  " " <<pago_estacionamiento(12)<< " " <<endl;
     cout << "25       " << " " << "0              "<<  " " <<pago_estacionamiento(25)<< " " <<endl;
+    
+    cout << "Table BVA" << endl;
+    cout << "Test data" << " " << "Expected result" << " " << "Actual result" << " " << "Pass/Fail" << endl;
+
+    cout << "0        " << " " << "0              "<<  " " <<pago_estacionamiento(0)<< " " <<endl;
+    cout << "1        " << " " << "10             "<<  " " <<pago_estacionamiento(1)<< " " <<endl;
+    cout << "5        " << " " << "50             "<<  " " <<pago_estacionamiento(5)<< " " <<endl;
+    cout << "6        " << " " << "54             "<<  " " <<pago_estacionamiento(6)<< " " <<endl;
+    cout << "11       " << " " << "90             "<<  " " <<pago_estacionamiento(11)<< " " <<endl;
+    cout << "24       " << " " << "168            "<<  " " <<pago_estacionamiento(24)<< " " <<endl;
+    cout << "25       " << " " << "0              "<<  " " <<pago_estacionamiento(25)<< " " <<endl;
 }
 
 int main()
